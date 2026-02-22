ba18d40a3547bd94f53bac8ad14d282ac40cceb3
Shailja <sc5011@gmail.com>
2026-02-23 01:42:20 +0530

    UC1 Print OOPS banner

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,14 @@
 class HelloApp {
+
+    static void printOOPS() {
+        System.out.println("OOOO   OOOO   PPPP   SSSS");
+        System.out.println("O  O   O  O   P  P   S");
+        System.out.println("O  O   O  O   PPPP   SSSS");
+        System.out.println("O  O   O  O   P         S");
+        System.out.println("OOOO   OOOO   P     SSSS");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello OOPS Banner App");
+        printOOPS();
     }
 }
