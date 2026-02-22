9ef51730a2b366ee8a2800fddd6d46cbdb8b5808
Shailja <sc5011@gmail.com>
2026-02-23 02:31:02 +0530

    UC4 Array Loop implementation

--- HelloApp.java
+++ HelloApp.java
@@ -1,19 +1,19 @@
 class HelloApp {
 
-    static void printOops() {
-
-        String banner = String.join("\n",
-            " OOOO   OOOO   PPPP    SSSS ",
-            "O    O O    O  P   P  S     ",
-            "O    O O    O  PPPP    SSS  ",
-            "O    O O    O  P          S ",
-            " OOOO   OOOO   P      SSSS  "
-        );
-
-        System.out.println(banner);
-    }
-
     public static void main(String[] args) {
-        printOops();
+
+        String[] lines = new String[7];
+
+        lines[0] = String.join("", " ****   ****   ******   ****** ");
+        lines[1] = String.join("", "**  ** **  ** **    ** **      ");
+        lines[2] = String.join("", "**  ** **  ** **    ** *****   ");
+        lines[3] = String.join("", "**  ** **  ** **    ** **      ");
+        lines[4] = String.join("", "**  ** **  ** **    ** **      ");
+        lines[5] = String.join("", "**  ** **  ** **    ** **      ");
+        lines[6] = String.join("", " ****   ****   ******   ****** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
