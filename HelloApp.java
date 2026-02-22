b4478026d5adcd6395a616c1e20b8709f78340ee
Shailja <sc5011@gmail.com>
2026-02-23 02:19:50 +0530

    UC3 StringJoin implementation

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,19 @@
 class HelloApp {
+
+    static void printOops() {
+
+        String banner = String.join("\n",
+            " OOOO   OOOO   PPPP    SSSS ",
+            "O    O O    O  P   P  S     ",
+            "O    O O    O  PPPP    SSS  ",
+            "O    O O    O  P          S ",
+            " OOOO   OOOO   P      SSSS  "
+        );
+
+        System.out.println(banner);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello OOPS Banner App");
+        printOops();
     }
 }
